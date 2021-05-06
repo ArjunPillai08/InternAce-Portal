2d76cca5d4add229f3b8b2754edcbc089514bf46
Arjun Pillai <arjun.pillai.08@gmail.com>
2021-05-06 08:57:35 -0400

    Changes

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,3 @@
 const SERVER_URL = 'https://internace.herokuapp.com/';
-const WEB_URL = 'https://internacefront-end.herokuapp.com';
+// const WEB_URL = 'https://internacefront-end.herokuapp.com'; //
+const WEB_URL = 'localhost:3002';
