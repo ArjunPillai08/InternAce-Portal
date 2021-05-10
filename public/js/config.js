58ef0ba7f8f26fc4c555f04cd9b93e1c4752e745
Arjun Pillai <arjunpillai@Arjuns-MacBook-Air.local>
2021-05-10 10:28:08 -0400

    Changes

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,2 @@
 const SERVER_URL = 'https://internace.herokuapp.com/';
-<<<<<<< HEAD
 const WEB_URL = 'http://localhost:3002';
-=======
-// const WEB_URL = 'https://internacefront-end.herokuapp.com'; //
-const WEB_URL = 'localhost:3002';
->>>>>>> d507997f24f3f47a904a0d4b5472a8a45b4aa452
